c4af983b2c4b5273d890f233b8f535d6ab5859d4
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2020-12-03 23:52:28 +0200

    poll events

--- kong/db/declarative/init.lua
+++ kong/db/declarative/init.lua
@@ -821,7 +821,7 @@ do
       ok, err = kong.worker_events.post("declarative", "flip_config", default_ws)
       if not ok then
         ngx.shared.kong:delete(DECLARATIVE_FLIPS_NAME)
-        return nil, "failed to flip declarative config cache pages: " .. (err or ok)
+        return nil, "failed to flip declarative config cache pages: " .. (err or "unknown error")
       end
 
     else
